19111d9b30c135475e46c7542c5c2f9d27f45dc0
Paul Kuzich <pkuzich@gmail.com>
2014-10-16 20:38:31 -0500

    add library module and wide button class

---
+++ library/src/main/java/com/fiumelabs/ui/WideButton.java
@@ -0,0 +1,17 @@
+package com.fiumelabs.ui;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+/**
+ * Created by paulkuzich on 10/16/14.
+ */
+public class WideButton extends ImageView {
+
+    public static final String TAG = WideButton.class.getSimpleName();
+
+    public WideButton(Context context){
+        super(context);
+    }
+
+}
