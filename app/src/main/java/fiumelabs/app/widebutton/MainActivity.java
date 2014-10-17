55a562fb8b750c0101210ac4680ab6ba1edbe6d9
Paul Kuzich <pkuzich@gmail.com>
2014-10-16 20:51:41 -0500

    add library

--- app/src/main/java/fiumelabs/app/widebutton/MainActivity.java
+++ app/src/main/java/fiumelabs/app/widebutton/MainActivity.java
@@ -1,7 +1,6 @@
 package fiumelabs.app.widebutton;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,8 +8,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
+import com.fiumelabs.ui.WideButton;
 
 
 public class MainActivity extends Activity {
@@ -58,6 +57,9 @@ public class MainActivity extends Activity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            WideButton button = (WideButton) rootView.findViewById(R.id.button0);
+
             return rootView;
         }
     }
--- library/src/main/java/com/fiumelabs/ui/WideButton.java
+++ library/src/main/java/com/fiumelabs/ui/WideButton.java
@@ -1,6 +1,8 @@
 package com.fiumelabs.ui;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.widget.ImageView;
 
 /**
@@ -10,8 +12,42 @@ public class WideButton extends ImageView {
 
     public static final String TAG = WideButton.class.getSimpleName();
 
+    private int width;
+    private int height;
+
+    private Paint buttonPaint;
+
+
     public WideButton(Context context){
         super(context);
+        initialize(context);
+
+    }
+
+    protected void initialize(Context context){
+
+        buttonPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        buttonPaint.setStyle(Paint.Style.FILL);
+
+        this.setFocusable(true);
+        this.setScaleType(ScaleType.CENTER_INSIDE);
+        setClickable(true);
+
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawRect(0, 0, width, height, buttonPaint);
+
+        super.onDraw(canvas);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        width = w;
+        height = h;
     }
 
 }
